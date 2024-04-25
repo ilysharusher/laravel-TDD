a8cbe8a2c0c953d3924eb9d5b9f80233789bbf20
Ilya <illyaetoilysha@gmail.com>
2024-04-25 16:32:53 +0300

    Test for API validation error for 'title' field in api.posts.store route

--- tests/Feature/Api/PostTest.php
+++ tests/Feature/Api/PostTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature\Api;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Testing\File;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
@@ -16,6 +15,8 @@ class PostTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->withHeader('Accept', 'application/json');
 
         Storage::fake('public');
     }
@@ -39,19 +40,43 @@ class PostTest extends TestCase
             'image' => File::image('image.jpg'),
         ];
 
-        $this->postJson(route('api.posts.store'), $data)
-            ->assertJson([
-                'title' => $data['title'],
-                'content' => $data['content'],
-                'image' => 'images/' . $data['image']->hashName(),
-            ]);
-
-        $this->assertDatabaseHas('posts', [
+        $dataCheck = [
             'title' => $data['title'],
             'content' => $data['content'],
             'image' => 'images/' . $data['image']->hashName(),
-        ]);
+        ];
+
+        $this->postJson(route('api.posts.store'), $data)
+            ->assertJson($dataCheck);
+
+        $this->assertDatabaseHas('posts', $dataCheck);
 
         Storage::disk('public')->assertExists('images/' . $data['image']->hashName());
     }
+
+    public function test_attribute_title_is_required_in_storing_posts()
+    {
+        $this->signIn();
+
+        $data = [
+            'title' => '',
+        ];
+
+        $this->post(route('api.posts.store'), $data)
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('title');
+    }
+
+    /*public function test_attribute_image_is_image_file_in_storing_posts()
+    {
+        $this->signIn();
+
+        $data = [
+            'title' => 'Title',
+            'image' => 'not-an-image-file',
+        ];
+
+        $results = $this->post(route('posts.store'), $data);
+        $results->assertSessionHasErrors('image');
+    }*/
 }
